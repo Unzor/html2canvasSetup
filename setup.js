0f01880c0870375b1c0e2b8a347d4df8796c8a9c
Unzor <71789103+Unzor@users.noreply.github.com>
2020-10-13 15:17:54 -0500

    Update setup.js

--- setup.js
+++ setup.js
@@ -1,7 +1,10 @@
 var btn = document.createElement("script");
 btn.src="https://makitweb.com/demo/html2canvas/html2canvas-master/dist/html2canvas.js"
 document.body.appendChild(btn);
-
+  var canvas = document.createElement("canvas");
+canvas.width="1300"
+canvas.height="629"
+document.body.appendChild(canvas);
           function screenshot(){
                 html2canvas(document.body).then(function(canvas) {
            
